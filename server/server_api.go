f7b974375897c179867fcb483410115b8e4a757e
maspodil <62073132+maspodil@users.noreply.github.com>
2020-03-11 23:19:24 +0530

    Update server_api.go

--- server/server_api.go
+++ server/server_api.go
@@ -11,7 +11,7 @@ import (
 	"github.com/anacrolix/torrent"
 	"github.com/anacrolix/torrent/metainfo"
 
-	"github.com/adctea/tavy/engine"
+	"github.com/maspodil/scrreenj/engine"
 )
 
 func (s *Server) api(r *http.Request) error {
